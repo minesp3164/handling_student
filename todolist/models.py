0eb9c17c554fe3c172f78af124c4488df6a6d7c7
minesp3164 <lhd000721@gmail.com>
2023-07-21 14:37:00 +0900

    feat: todolist app 생성 및 model 추가

---
+++ todolist/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class TodoList(models.Model):
+    content = models.CharField(max_length=250)
+    checked = models.BooleanField(default=False)
+    created_at = models.DateField(auto_now=True)
+    edited_at = models.DateField(auto_now_add=True)
+    author = models.ForeignKey('user.User', verbose_name="작성자", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.content
