8e205093f4e1c5022df11e4f0fe3b7cbf484f687
minesp3164 <lhd000721@gmail.com>
2023-07-22 18:02:02 +0900

    feat: 회원가입 과 로그인 간 이동 and placeholder 조정

--- user/forms.py
+++ user/forms.py
@@ -3,22 +3,36 @@ from django import forms
 
 class LoginForm(forms.Form):
     username = forms.CharField(min_length=3,
-                        widget=forms.TextInput(
-                            attrs={
-                                "placeholder": "사용자 명을 입력하여 주시오."
+                            widget=forms.TextInput(
+                                attrs={
+                                    "placeholder": "사용자 명을 입력하여 주시오."
                                 },
                             ),
-                        )
+                            )
     password = forms.CharField(min_length=4,
-                                widget=forms.PasswordInput(
-                                    attrs={
+                            widget=forms.PasswordInput(
+                                attrs={
                                     "placeholder": "비밀번호를 입력하여 주시오."
                                 },
                             ),
-                        )
+                            )
 
 
 class SignupForm(forms.Form):
-    username = forms.CharField()
-    password1 = forms.CharField(widget=forms.PasswordInput)
-    password2 = forms.CharField(widget=forms.PasswordInput)
+    username = forms.CharField(widget=forms.TextInput(
+        attrs={
+            "placeholder": "사용하실 닉네임을 입력하시오"
+        },
+    ), )
+    password1 = forms.CharField(widget=forms.PasswordInput(
+        attrs={
+            "placeholder": "사용할 비밀번호를 입력하여 주십시오"
+        },
+    ),
+    )
+    password2 = forms.CharField(widget=forms.PasswordInput(
+        attrs={
+            "placeholder": "사용할 비밀번호를 입력하여 주십시오"
+        },
+    ),
+    )
