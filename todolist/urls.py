66206025cb104ec2de0ac8985e19850a06b0541d
minesp3164 <lhd000721@gmail.com>
2023-08-02 17:11:31 +0900

    feat: todolist edit view 완성 및 template 연결

--- todolist/urls.py
+++ todolist/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import todolist_view, todolist_add, todolist_detail, todolist_checked,todolist_delete
+from .views import todolist_view, todolist_add, todolist_detail, todolist_checked, todolist_delete, todolist_edit
 
 urlpatterns = [
     path('',todolist_view),
     path('detail/<int:todolist_id>/',todolist_detail),
     path('add/',todolist_add),
     path('<int:todolist_id>/checked/',todolist_checked),
+    path('<int:todolist_id>/edit',todolist_edit),
     path('<int:todolist_id>/delete/',todolist_delete),
 ]
--- todolist/views.py
+++ todolist/views.py
@@ -1,7 +1,7 @@
 from django.contrib import messages
 from django.http import HttpResponseRedirect
-from django.shortcuts import render, get_object_or_404
-from django.urls import reverse
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
 
 from .forms import TodoListForm
 from .models import TodoList
@@ -57,3 +57,24 @@ def todolist_delete(request,todolist_id):
         todolist.delete()
     return HttpResponseRedirect("/todolist/")
 
+
+def todolist_edit(request, todolist_id):
+    todolist = get_object_or_404(TodoList,pk=todolist_id)
+    if request.user != todolist.author:
+        messages.error(request, '수정 권한이 없습니다!')
+        return redirect('/todolist/')
+    if request.method == "POST":
+        form = TodoListForm(request.POST, instance=todolist)
+        if form.is_valid():
+            todolist = form.save(commit=False)
+            todolist.edited_at = timezone.now()
+            todolist.save()
+            return HttpResponseRedirect(f'/todolist/detail/{todolist.id}')
+        else:
+            form.add_error("모든 것을 채우시오")
+    else:
+        form = TodoListForm(instance=todolist)
+    context = {
+        "form": form
+    }
+    return render(request,'todolist/edit.html',context)
